8d3be5c952100c472e0d9d08c1f9cc0a7422f30e
sangavi002 <sangavibabu007@gmail.com>
2024-05-13 18:29:06 +0530

    Implement lululemon website clone

--- functions/jsonServer.js
+++ functions/jsonServer.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const serverless = require('serverless-http');
-const jsonServer = require('json-server');
-const app = express();
-const router = jsonServer.router('db.json'); // Path to your JSON data file
-const middlewares = jsonServer.defaults();
+// jsonServer.js
+const fetch = require('node-fetch');
 
-app.use('/api', router); // Proxy requests to /api to your JSON server
-app.use(middlewares);
+exports.handler = async (event, context) => {
+  const response = await fetch('http://localhost:3000' + event.path);
+  const data = await response.json();
 
-module.exports = app;
-module.exports.handler = serverless(app);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
+};
